cdb9d3e5c94fc86f643f645c358e27c8f0d9f67a
iluretar <robertocuellarg@gmail.com>
2015-10-29 03:05:36 -0400

    *Add examples to sample app

--- sample/src/main/java/com/mobilebits/toggleimagebutton/example/MainActivity.java
+++ sample/src/main/java/com/mobilebits/toggleimagebutton/example/MainActivity.java
@@ -2,12 +2,36 @@ package com.mobilebits.toggleimagebutton.example;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.LinearLayout;
+
+import com.mobilebits.toggleimagebutton.ToggleImageButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ToggleImageButton mHorizontalToggle;
+    private LinearLayout mHorizontalLayout;
+
+    private ToggleImageButton mVerticalToggle;
+    private LinearLayout mVerticalLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mHorizontalToggle = (ToggleImageButton)findViewById(R.id.horizontal_toggle);
+        mHorizontalLayout = (LinearLayout)findViewById(R.id.horizontal_layout);
+
+        mVerticalToggle = (ToggleImageButton)findViewById(R.id.vertical_toggle);
+        mVerticalLayout = (LinearLayout)findViewById(R.id.vertical_layout);
+
+        mVerticalToggle.setAnimDirection(ToggleImageButton.ANIM_DIRECTION_HORIZONTAL);
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        mHorizontalToggle.setParentSize(mHorizontalLayout.getHeight());
+        mVerticalToggle.setParentSize(mVerticalLayout.getWidth());
     }
 }
